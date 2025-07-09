98096a10430acaaf43e8bd50468b3fc37e409b88
andychen <achen846@students.cs.ubc.ca>
2025-07-08 23:48:59 -0700

    Second Commit

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -5,3 +5,7 @@ public class Main {
         System.out.println("Welcome to my project!");
     }
 }
+
+// This is a comment that I added after connecting my starter files to my
+// personal repository on GitHub, and then cloned the repo back into a different
+// project on my own laptop. I am now going to commit this change back.
