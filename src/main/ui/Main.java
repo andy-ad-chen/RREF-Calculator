bb3888231464bb660e0b2cdfa5d05694ed4607e7
andychen <achen846@students.cs.ubc.ca>
2025-07-13 12:31:01 -0700

    UI package set up

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,11 +1,8 @@
 package ui;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Welcome to my project!");
+    public static void main(String[] args) {
+        new MatrixReducerApp();
     }
 }
 
-// This is a comment that I added after connecting my starter files to my
-// personal repository on GitHub, and then cloned the repo back into a different
-// project on my own laptop. I am now going to commit this change back.
---
+++ src/main/ui/MatrixReducerApp.java
@@ -0,0 +1,13 @@
+package ui;
+
+public class MatrixReducerApp {
+
+    // EFFECTS: runs the Matrix Reducer App
+    public MatrixReducerApp() {
+        runApp();
+    }
+
+    public void runApp() {
+        // stub
+    }
+}
