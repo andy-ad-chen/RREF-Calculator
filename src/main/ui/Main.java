c1ee2a6ecd2076993964e48762ec48f3e3f35dc9
andychen <achen846@students.cs.ubc.ca>
2025-07-14 02:12:58 -0700

    Added console application to add and to view matrices in a collection

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -10,54 +10,54 @@ public class Main {
     public static void main(String[] args) {
         new MatrixReducerApp();
 
-         Matrix testMatrix;
-         ArrayList<Float> listFloatTypical1 = new ArrayList<>();
-         ArrayList<Float> listFloatTypical2 = new ArrayList<>();
-         ArrayList<Float> listFloatTypical3 = new ArrayList<>();
+        //  Matrix testMatrix;
+        //  ArrayList<Float> listFloatTypical1 = new ArrayList<>();
+        //  ArrayList<Float> listFloatTypical2 = new ArrayList<>();
+        //  ArrayList<Float> listFloatTypical3 = new ArrayList<>();
 
-         ArrayList<Row> typicalTestVals = new ArrayList<>();
+        //  ArrayList<Row> typicalTestVals = new ArrayList<>();
 
-         listFloatTypical1.add(1.0f);
-         listFloatTypical1.add(0.0f);
-         listFloatTypical1.add(0.0f);
-         listFloatTypical1.add(0.0f);
-         listFloatTypical1.add(0.0f);
-         listFloatTypical1.add(0.0f);
+        //  listFloatTypical1.add(1.0f);
+        //  listFloatTypical1.add(5.0f);
+        //  listFloatTypical1.add(3.0f);
+        //  listFloatTypical1.add(5.0f);
+        //  listFloatTypical1.add(6.0f);
+        //  listFloatTypical1.add(4.0f);
    
 
-         listFloatTypical2.add(0.0f);
-         listFloatTypical2.add(10.0f);
-         listFloatTypical2.add(3.0f);
-         listFloatTypical2.add(0.0f);
-         listFloatTypical2.add(1.0f);
-         listFloatTypical2.add(0.0f);
+        //  listFloatTypical2.add(5.0f);
+        //  listFloatTypical2.add(10.0f);
+        //  listFloatTypical2.add(3.0f);
+        //  listFloatTypical2.add(2.0f);
+        //  listFloatTypical2.add(1.0f);
+        //  listFloatTypical2.add(1.0f);
     
 
-         listFloatTypical3.add(0.0f);
-         listFloatTypical3.add(4.0f);
-         listFloatTypical3.add(0.0f);
-         listFloatTypical3.add(0.0f);
-         listFloatTypical3.add(0.0f);
-         listFloatTypical3.add(0.0f);
+        //  listFloatTypical3.add(1.0f);
+        //  listFloatTypical3.add(4.0f);
+        //  listFloatTypical3.add(7.0f);
+        //  listFloatTypical3.add(4.0f);
+        //  listFloatTypical3.add(6.0f);
+        //  listFloatTypical3.add(4.0f);
  
 
-         Row typicalCaseRow1;
-         Row typicalCaseRow2;
-         Row typicalCaseRow3;
+        //  Row typicalCaseRow1;
+        //  Row typicalCaseRow2;
+        //  Row typicalCaseRow3;
 
-         typicalCaseRow1 = new Row(6, listFloatTypical1);
-         typicalCaseRow2 = new Row(6, listFloatTypical2);
-         typicalCaseRow3 = new Row(6, listFloatTypical3);
+        //  typicalCaseRow1 = new Row(6, listFloatTypical1);
+        //  typicalCaseRow2 = new Row(6, listFloatTypical2);
+        //  typicalCaseRow3 = new Row(6, listFloatTypical3);
 
-         typicalTestVals.add(typicalCaseRow1);
-         typicalTestVals.add(typicalCaseRow2);
-         typicalTestVals.add(typicalCaseRow3);
+        //  typicalTestVals.add(typicalCaseRow1);
+        //  typicalTestVals.add(typicalCaseRow2);
+        //  typicalTestVals.add(typicalCaseRow3);
 
-         testMatrix = new Matrix(typicalTestVals, 6, "myMatrix", "this is a matrix desc");
-         testMatrix.computeRedRef();
-         for (Row r : testMatrix.getRedRefRows()) {
-             System.out.println(r.getFloatArray());
-         }
+        //  testMatrix = new Matrix(typicalTestVals, 6, "myMatrix", "this is a matrix desc");
+        //  testMatrix.computeRedRef();
+        //  for (Row r : testMatrix.getRedRefRows()) {
+        //      System.out.println(r.getFloatArray());
+        //  }
     }
 }
 
--- src/main/ui/MatrixReducerApp.java
+++ src/main/ui/MatrixReducerApp.java
@@ -1,13 +1,133 @@
 package ui;
 
+import model.Matrix;
+import model.MatrixList;
+import model.Row;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class MatrixReducerApp {
+
+    private MatrixList matrices = new MatrixList();
+    private Scanner input;
 
     // EFFECTS: runs the Matrix Reducer App
     public MatrixReducerApp() {
         runApp();
     }
 
-    public void runApp() {
-        // stub
+    // MODIFIES: this
+    // EFFECTS: processes user input
+    private void runApp() {
+        boolean keepGoing = true;
+        String command = null;
+
+        init();
+
+        while (keepGoing) {
+            displayMenu();
+            command = input.next();
+            command = command.toLowerCase();
+
+            if (command.equals("q")) {
+                keepGoing = false;
+            } else {
+                processCommand(command);
+            }
+        }
+
+        System.out.println("Bye!");
+    }
+
+
+    // MODIFIES: this
+    // EFFECTS: initializes accounts
+    private void init() {
+        input = new Scanner(System.in);
+      //  input.useDelimiter("\r?\n|\r");
+    }
+
+
+    // EFFECTS: displays menu of options to user
+    private void displayMenu() {
+        System.out.println("Select from:");
+        System.out.println("a: add matrix:   ");
+        System.out.println("v: view list of matrices:   ");
+        System.out.println("s: select and detail view a matrix:   ");
+        System.out.println("q: quit:   ");
+    }
+
+    // MODIFIES: this
+    // EFFECTS: processes user command
+    private void processCommand(String command) {
+        if (command.equals("a")) {
+            addingMatrix();
+        } else if (command.equals("v")) {
+            viewList();
+        } else if (command.equals("s")) {
+           selectMatrixIndex();
+        } else {
+            System.out.println("Selection not valid...");
+        }
+    }
+
+    // MODIFIES: this
+    // EFFECTS: allows a user to specify a matrix
+    private void addingMatrix() {
+        System.out.print("Enter the number of rows of your matrix:   ");
+        int height = input.nextInt();
+        System.out.print("Enter the number of columns of your matrix:   ");
+        int width = input.nextInt();
+        System.out.print("Enter the name of your matrix:   ");
+        String name = input.next();
+        System.out.print("Enter the description of your matrix:   ");
+        String desc = input.next();
+        ArrayList<Float> listOfVals = new ArrayList();
+        specifyMatrix(height, width, name, desc);
+    }
+
+    // MODIFIES: this
+    // EFFECTS: allows user to specify a matrix
+    void specifyMatrix(int height,int width, String name, String desc) {
+        ArrayList<Row> listOfRows= new ArrayList<>();
+        for (int j = 0; j < height; j++) {
+            ArrayList<Float> listOfVals = new ArrayList();
+            for (int i = 0; i < width; i++) {
+                System.out.print("Enter the next entry of your matrix, in the " + (j + 1) + " -th row:  ");
+                float val = input.nextFloat();
+                listOfVals.add(val);
+            }
+            Row row = new Row(width, listOfVals);
+            listOfRows.add(row);
+        }
+        Matrix matrix = new Matrix(listOfRows, width, name, desc);
+        matrices.addMatrix(matrix);
+    }
+
+    // EFFECTS: shows users a list of matrice's names alongside their number
+    void viewList() {
+        for (int i = 0; i < matrices.getMatrices().size(); i++) {
+            System.out.println("#" + (i + 1) + " - " + matrices.getMatrices().get(i).getMatrixName());
+        }
+    }
+
+    // EFFECTS: allows users to inspect a matrix
+    void selectMatrixIndex() {
+        System.out.println("Matrix number?:  ");
+        int val = input.nextInt();
+        Matrix matrix = matrices.getMatrices().get(val - 1);
+        System.out.println("Matrix is called:" + matrix.getMatrixName());
+        System.out.println(matrix.getMatrixDesc());
+        for (int i = 0; i < matrix.getMatrixRows().size(); i++) {
+            System.out.println(matrix.getMatrixRows().get(i).getFloatArray());
+        }
+        System.out.println("Matrix in Reduced Row Echelon Form is:");
+        matrix.computeRedRef();
+        for (int i = 0; i < matrix.getRedRefRows().size(); i++) {
+            System.out.println(matrix.getRedRefRows().get(i).getFloatArray());
+        }
     }
 }
+
+
